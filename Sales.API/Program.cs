b7c694e27f657ecd5b31d7addcbbcdacc2c37f24
Olga Susana Carrasco Quezada <susy760213@hotmail.com>
2023-02-23 20:24:16 -0600

    Add CountriesController

--- Sales.API/Program.cs
+++ Sales.API/Program.cs
@@ -28,4 +28,11 @@ app.UseAuthorization();
 
 app.MapControllers();
 
+app.UseCors(x => x
+    .AllowAnyMethod()
+    .AllowAnyHeader()
+    .SetIsOriginAllowed(origin => true)
+    .AllowCredentials());
+
+
 app.Run();
